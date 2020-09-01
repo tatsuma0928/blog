9cd3f1b5b9906477526d1f297b9c32c396db8e52
tatsuma0928 <tatsuma.sano@icloud.com>
2020-09-01 14:22:58 +0900

    ビューファイル作成

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    //
+    // /
+    //  ブログ一覧を表示する
+    //  @return view
+
+    public function showList() 
+    {
+        return view('blog.list');
+    }
 }
---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,29 @@
+<!--
+①共通テンプレ
+②共通ヘッダーを作る
+③共通フッターを作る
+④共通テンプレを景勝したリストを作る
+-->
+@extends('layout')
+@section('title', 'ブログ一覧')
+@section('content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <h2>ブログ記事一覧</h2>
+        <table class="table table-striped">
+            <tr>
+               <th>記事番号</th>
+               <th>日付</th>
+               <th>タイトル</th>
+               <th></th>
+            </tr>
+            <tr>
+               <td>1</td>
+               <td>2020/06/30</td>
+               <td>テスト</td>
+               <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+@endsection
